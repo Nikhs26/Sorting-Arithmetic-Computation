3ee11dd44fdf070ceb553338443657d4512695c3
Nikhil Gupta <nikhilgahoi1731997@gmail.com>
2022-01-07 21:03:18 +0530

    Assume Part time Hour is 8

--- arithmetic.sh
+++ arithmetic.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 
-read -p "Enter three numbers: " a b c
-
-z=$((a+b*c))
-echo "the output is: $z"
+read -p "Enter First number: " a
+read -p "Enter Second number: " b
+read -p "Enter Third number: " c
+echo "the result is: "$((a=b*c))
+part_time=8
+echo "Assume part time hour is: $part_time"
