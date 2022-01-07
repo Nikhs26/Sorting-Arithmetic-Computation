61251380996a2aee1bf58f95e435b65cf46c1a3c
Nikhil Gupta <nikhilgahoi1731997@gmail.com>
2022-01-07 20:48:06 +0530

    Compute a + b * c operation

--- arithmetic.sh
+++ arithmetic.sh
@@ -1,8 +1,6 @@
-#Write program to take three inputs –a, b & c
-
 #! /bin/bash
 
 read -p "Enter three numbers: " a b c
-echo "First number is: "$a
-echo "Second number is: "$b
-echo "Third number is: "$c
+
+z=$((a+b*c))
+echo "the output is: $z"
