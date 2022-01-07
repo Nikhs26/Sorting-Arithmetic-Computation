f757142e58b90b0578315fb6bd1753522b2de604
Nikhil Gupta <nikhilgahoi1731997@gmail.com>
2022-01-07 22:09:17 +0530

    Read the values from the Dictionary into the array

--- arithmetic.sh
+++ arithmetic.sh
@@ -6,10 +6,15 @@ read -p "Enter First number: " a
 read -p "Enter Second number: " b
 read -p "Enter Third number: " c
 
-oprn[key1]=$((a*b+c))
-oprn[key2]=$((c+a/b))
-oprn[key3]=$((a%b+c))
+oprn[0]=$((a*b+c))
+oprn[1]=$((c+a/b))
+oprn[2]=$((a%b+c))
 
-echo "the result of First oprn is: "${oprn[key1]}
-echo "the result of second oprn is: "${oprn[key2]}
-echo "the result of third oprn is: "${oprn[key3]}
+echo "the result of First oprn is: "${oprn[0]}
+echo "the result of second oprn is: "${oprn[1]}
+echo "the result of third oprn is: "${oprn[2]}
+
+for val in "${oprn[@]}";
+do
+	echo "array value is: $val"
+done
