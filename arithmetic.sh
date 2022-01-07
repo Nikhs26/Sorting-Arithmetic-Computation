002837c52744abd47751fd0b390a37d7b7ce7bdd
Nikhil Gupta <nikhilgahoi1731997@gmail.com>
2022-01-07 21:32:45 +0530

    Store the results in a Dictionary for every Computation

--- arithmetic.sh
+++ arithmetic.sh
@@ -1,6 +1,15 @@
 #! /bin/bash
 
+declare -A oprn
+
 read -p "Enter First number: " a
 read -p "Enter Second number: " b
 read -p "Enter Third number: " c
-echo "the result is: "$((a%b+c))
+
+oprn[key1]=$((a*b+c))
+oprn[key2]=$((c+a/b))
+oprn[key3]=$((a%b+c))
+
+echo "the result of First oprn is: "${oprn[key1]}
+echo "the result of second oprn is: "${oprn[key2]}
+echo "the result of third oprn is: "${oprn[key3]}
